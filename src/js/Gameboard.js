caa5e1bbb241311bb9bfc0f432b1d8fd36f91691
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-03-21 17:49:14 +0530

    refactor(Gameboard): Minor changes to improve code accessibility

--- src/js/Gameboard.js
+++ src/js/Gameboard.js
@@ -1,14 +1,14 @@
 class Gameboard {
-  #attackedCoords;
+  #enemyAttacks;
 
   constructor(size) {
     this.size = size;
     // Set<Coord: string> - List of coordinates which have been attacked
-    this.#attackedCoords = new Set();
+    this.#enemyAttacks = new Set();
   }
 
   canPlaceShip(ship, coord) {
-    if (!Gameboard.#isValidCoord(coord)) return false;
+    if (!Array.isArray(coord)) return false;
 
     return ship.orientation === "vertical"
       ? this.#isVerticallyBounded(coord, ship.length)
@@ -16,15 +16,14 @@ class Gameboard {
   }
 
   canAttack(coord) {
-    if (!Gameboard.#isValidCoord(coord) || this.#hasAttacked(coord))
-      return false;
+    if (!Array.isArray(coord) || this.#hasAttacked(coord)) return false;
 
-    this.#attackedCoords.add(coord.toString());
+    this.#enemyAttacks.add(coord.toString());
     return true;
   }
 
   #hasAttacked(coord) {
-    return this.#attackedCoords.has(coord.toString());
+    return this.#enemyAttacks.has(coord.toString());
   }
 
   #isHorizontallyBounded(coord, length) {
@@ -35,10 +34,6 @@ class Gameboard {
   #isVerticallyBounded(coord, length) {
     const offsetY = coord[1] + length - 1;
     return offsetY >= 0 && offsetY < this.size;
-  }
-
-  static #isValidCoord(coord) {
-    return Array.isArray(coord);
   }
 
   static new({ size = 10 }) {
