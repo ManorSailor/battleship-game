510198a19af51c3be6dfb0a88ead6cad42ea6706
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-03-23 12:21:10 +0530

    feat(Player): Implement Player base class

---
+++ src/__tests__/Player.test.js
@@ -0,0 +1,35 @@
+import Player from "../js/Player";
+
+describe("Player", () => {
+  const player = Player.new("Test Player");
+
+  it("has proper public interface", () => {
+    expect(player).toMatchObject({
+      attack: expect.any(Function),
+      placeShip: expect.any(Function),
+      receiveAttack: expect.any(Function),
+      hasDeployedFleet: expect.any(Function),
+      hasFleetSunk: expect.any(Function),
+    });
+  });
+
+  it("has a getter for board", () => {
+    const getter = jest.spyOn(player, "board", "get");
+    const { ...board } = player.board;
+
+    expect(getter).toHaveBeenCalled();
+    expect(board).toMatchObject({
+      size: expect.any(Number),
+      hasShipAt: expect.any(Function),
+    });
+  });
+});
+
+describe("Player: autoplace", () => {
+  const player = Player.new("Test Player");
+
+  it("successfully autoplace the ships", () => {
+    player.autoplace();
+    expect(player.hasDeployedFleet()).toBe(true);
+  });
+});
---
+++ src/js/Player.js
@@ -0,0 +1,76 @@
+import Gameboard from "./Gameboard";
+import ShipManager from "./ShipManager";
+
+class Player {
+  #board;
+  #shipManager;
+
+  constructor(name) {
+    this.name = name;
+    this.#board = Gameboard.new({ size: 10 });
+    this.#shipManager = ShipManager.new();
+  }
+
+  get board() {
+    return {
+      size: this.#board.size,
+      hasShipAt: this.#shipManager.hasShipAt.bind(this.#shipManager),
+    };
+  }
+
+  attack(enemy, coord) {
+    return enemy.receiveAttack(coord);
+  }
+
+  receiveAttack(coord) {
+    const boardStatus = this.#board.canEnemyAttack(coord);
+
+    if (boardStatus.attackSuccess) {
+      const shipStatus = this.#shipManager.attackShipAt(coord);
+      return { ...boardStatus, ...shipStatus };
+    }
+
+    return boardStatus;
+  }
+
+  placeShip(shipName, coord) {
+    const ship = this.#shipManager.getShip(shipName);
+
+    if (ship && this.#board.canPlaceShip(ship, coord)) {
+      return this.#shipManager.deployShip(shipName, coord);
+    }
+
+    return false;
+  }
+
+  hasDeployedFleet() {
+    return this.#shipManager.hasDeployedFleet();
+  }
+
+  hasFleetSunk() {
+    return this.#shipManager.hasFleetSunk();
+  }
+
+  autoplace() {
+    // TODO: Make sure to reset state first
+    let ship = this.#shipManager.getRandomShip();
+
+    while (!this.#shipManager.hasDeployedFleet()) {
+      const coord = [
+        Math.floor(Math.random() * this.#board.size),
+        Math.floor(Math.random() * this.#board.size),
+      ];
+
+      const placed = this.placeShip(ship.name, coord);
+      if (placed) {
+        ship = this.#shipManager.getRandomShip();
+      }
+    }
+  }
+
+  static new(name) {
+    return new Player(name);
+  }
+}
+
+export default Player;
